784fe133819fd67428cabe26ff339bf5ec781768
Daniel Velazco <da27ve09@hotmail.com>
2018-03-07 17:18:16 -0400

    fix bug with database name

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://ds149268.mlab.com:49268/todoapp'
+  localhost: 'mongodb://localhost:27017/TodosApp',
+  mlab: 'mongodb://ds149268.mlab.com:49268/todosapp'
 };
 mongoose.connect( db.localhost || db.mlab);
 
