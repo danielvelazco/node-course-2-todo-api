e08172f5870505917e2868c4a635ce92547e5e07
Daniel Velazco <da27ve09@hotmail.com>
2018-03-07 17:42:24 -0400

    fix bug with database url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodosApp',
-  mlab: 'mongodb://ds149268.mlab.com:49268/todosapp'
+  mlab: 'mongodb://dvelazco:dv030406@ds259768.mlab.com:59768/todosapp'
 };
 mongoose.connect( db.localhost || db.mlab);
 
