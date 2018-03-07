bd88a1f59aaf2c4ce918e585a121fcdb7b398e4d
Daniel Velazco <da27ve09@hotmail.com>
2018-03-07 18:08:44 -0400

    fix bug with database url 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodosApp',
-  mlab: 'mongodb://dvelazco:dv030406@ds259768.mlab.com:59768/todosapp'
-};
-mongoose.connect( db.localhost || db.mlab);
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodosApp',
+//   mlab: 'mongodb://dvelazco:dv030406@ds259768.mlab.com:59768/todosapp'
+// };
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodosApp');
 
 //mongoose.connect('mongodb://localhost:27017/TodosApp');
 
