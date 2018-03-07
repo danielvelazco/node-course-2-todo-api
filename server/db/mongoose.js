a38e78e1a22761b7142b8450626822ba9add5523
Daniel Velazco <da27ve09@hotmail.com>
2018-03-07 18:24:31 -0400

    fixing some bugs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-// let db = {
-//   localhost: 'mongodb://localhost:27017/TodosApp',
-//   mlab: 'mongodb://dvelazco:dv030406@ds259768.mlab.com:59768/todosapp'
-// };
 mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodosApp');
-
-//mongoose.connect('mongodb://localhost:27017/TodosApp');
 
 module.exports = {mongoose};
